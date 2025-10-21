3a4d3f88e755728f0f3c0af811e43da52c0280e4
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-10-21 17:13:37 -0300

    zerando arquivo

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php 
 //Importando a classe Cliente
     require_once "src/Cliente.php";
-//Criando objetos (oi instâncias) à partir da Classe 
-    $clienteA= new Cliente();
-    $clienteB= new Cliente();
-//Atribuindo valores ao objeto $clienteA
-    $clienteA->name="Andre";
-    $clienteA->idade=30;
-    $clienteA->email="kururusaiaga@gmail.com";
-//Atribundo valores ao objero $clienteB
-    $clienteB->name="Mario";
-    $clienteB->idade=15;
-    $clienteB->email="mushurum@gmail.com";
+
 
 ?>
 
@@ -27,11 +17,7 @@
         <hr>
         <h2>Trabalhando com classes e objetos</h2>
         
-        <h3>Acessando/Lendo os dados dos Objetos</h3>
-        <?=$clienteA->monstraDados();?>
-        <hr>
-        <?=$clienteB->monstraDados();?>
 
-        <pre><?=var_dump($clienteA,$clienteB);?></pre>
+        
     </body>
 </html>
